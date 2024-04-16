a4f31b9f40d5232e538fb7d424b7d20a27f49a04
FlaviaV <flavia.voicuu@gmail.com>
2024-04-16 11:01:20 +0200

    define class

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fvoicu <fvoicu@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 10:53:36 by fvoicu            #+#    #+#             */
+/*   Updated: 2024/04/16 11:00:50 by fvoicu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_H
+# define FIXED_H
+
+class Fixed {
+  int               value;
+  static const int  frBits = 8;
+
+ public:
+  Fixed();
+  Fixed(const Fixed& other);
+  Fixed& operator=(const Fixed& other);
+  ~Fixed();
+  int getRawBits(void) const;
+  void setRawBits(int const raw);
+};
+
+#endif
