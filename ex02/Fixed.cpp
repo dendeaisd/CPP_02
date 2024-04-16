49b92cbd71a3e6aecebadc104b3c80fe3c4224a0
FlaviaV <flavia.voicuu@gmail.com>
2024-04-16 15:27:40 +0200

    copyed previous ex in here

---
+++ ex02/Fixed.cpp
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fvoicu <fvoicu@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 15:26:09 by fvoicu            #+#    #+#             */
+/*   Updated: 2024/04/16 15:26:11 by fvoicu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include "Fixed.hpp"
+
+Fixed::Fixed() : value(0) {
+  std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int number) {
+  std::cout << "Int constructor called" << std::endl;
+  value = number << frBits;
+}
+
+Fixed::Fixed(float number) {
+  std::cout << "Float constructor called" << std::endl;
+  value = roundf(number * (1 << frBits));
+}
+
+Fixed::Fixed(const Fixed& other) : value(other.value) {
+  std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed& other) {
+  if (this != &other) {
+    std::cout << "Copy assignment operator called" << std::endl;
+    this->value = other.getRawBits();
+  }
+  return *this;
+}
+
+Fixed::~Fixed() {
+  std::cout << "Destructor called" <<std::endl;
+}
+
+int Fixed::getRawBits(void) const {
+  std::cout << "getRawBits member function called" << std::endl;
+  return value;
+}
+
+void Fixed::setRawBits(int const raw) {
+  value = raw;
+}
+
+void Fixed::printOutput() {
+  std::cout << value << std::endl;
+  std::cout << frBits << std::endl;
+}
+
+float Fixed::toFloat(void) const {
+  return (float)value / (1 << frBits);
+}
+
+int Fixed::toInt(void) const {
+  return value >> frBits;
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& number){
+  os << number.toFloat();
+  return os;
+}
---
+++ ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fvoicu <fvoicu@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 15:26:42 by fvoicu            #+#    #+#             */
+/*   Updated: 2024/04/16 15:27:10 by fvoicu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+
+int main( void ) {
+Fixed a;
+Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+std::cout << a << std::endl;
+std::cout << ++a << std::endl;
+std::cout << a << std::endl;
+std::cout << a++ << std::endl;
+std::cout << a << std::endl;
+std::cout << b << std::endl;
+std::cout << Fixed::max( a, b ) << std::endl;
+return 0;
+}
