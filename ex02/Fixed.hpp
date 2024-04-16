c58ef6b766ed7cf49312bf8eb472b4a319755c6a
FlaviaV <flavia.voicuu@gmail.com>
2024-04-16 15:36:11 +0200

    completed class

--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: fvoicu <fvoicu@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 15:26:20 by fvoicu            #+#    #+#             */
-/*   Updated: 2024/04/16 15:26:25 by fvoicu           ###   ########.fr       */
+/*   Updated: 2024/04/16 15:35:37 by fvoicu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,31 @@ class Fixed {
   Fixed(const int number);
   Fixed(float number);
   Fixed(const Fixed& other);
-  Fixed& operator=(const Fixed& other);
   ~Fixed();
+
+  Fixed& operator=(const Fixed& other);
+  Fixed& operator>(const Fixed& other);
+  Fixed& operator<(const Fixed& other);
+  Fixed& operator>=(const Fixed& other);
+  Fixed& operator<=(const Fixed& other);
+  Fixed& operator==(const Fixed& other);
+  Fixed& operator!=(const Fixed& other);
+
+  Fixed& operator+(const Fixed& other);
+  Fixed& operator-(const Fixed& other);
+  Fixed& operator*(const Fixed& other);
+  Fixed& operator/(const Fixed& other);
+
+  Fixed& operator++();
+  Fixed& operator++(int);
+  Fixed& operator--();
+  Fixed& operator--(int);
+
+  static Fixed& min(Fixed& a, Fixed& b);
+  static Fixed& min(const Fixed& a, const Fixed& b);
+  static Fixed& max(Fixed& a, Fixed& b);
+  static Fixed& max(const Fixed& a, const Fixed& b);
+
   int getRawBits(void) const;
   void setRawBits(int const raw);
   void printOutput();
