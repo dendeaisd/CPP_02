4794e99d6c0bd018a67feb0032d2419e0f6e1802
FlaviaV <flavia.voicuu@gmail.com>
2024-04-16 15:23:32 +0200

    ex01

---
+++ ex01/Fixed.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fvoicu <fvoicu@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 13:50:31 by fvoicu            #+#    #+#             */
+/*   Updated: 2024/04/16 15:19:13 by fvoicu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef FIXED_H
+# define FIXED_H
+
+class Fixed {
+  int               value;
+  static const int  frBits = 8;
+
+ public:
+  Fixed();
+  Fixed(const int number);
+  Fixed(float number);
+  Fixed(const Fixed& other);
+  Fixed& operator=(const Fixed& other);
+  ~Fixed();
+  int getRawBits(void) const;
+  void setRawBits(int const raw);
+  void printOutput();
+  float toFloat(void) const;
+  int toInt(void) const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& number);
+#endif
