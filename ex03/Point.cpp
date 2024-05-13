232fb20c713c7e8973266a8ac1ec70f23c747177
FlaviaV <flavia.voicuu@gmail.com>
2024-05-13 20:11:15 +0200

    makfile done

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: fvoicu <fvoicu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 16:19:30 by fvoicu            #+#    #+#             */
-/*   Updated: 2024/05/12 16:37:14 by fvoicu           ###   ########.fr       */
+/*   Updated: 2024/05/13 19:28:26 by fvoicu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ Point::Point(float x, float y) : x(x), y(y) {}
 
 Point::Point(const Point& other) : x(other.x), y(other.y) {}
 
-Point& Point::operator=(const Point& other) {
-    // Since x and y are const, they cannot be assigned new values
-    // This is actually redundant in this scenario since we cannot assign const members
-    return *this;
-}
+// Point& Point::operator=(const Point& other) {
+//     // Since x and y are const, they cannot be assigned new values
+//     // This is actually redundant in this scenario since we cannot assign const members
+//     return *this;
+// }
 
 // Destructor
 Point::~Point() {}
