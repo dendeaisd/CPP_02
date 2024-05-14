e4c5f3550e706fb2a1041a8621da9be9b3259ce6
FlaviaV <flavia.voicuu@gmail.com>
2024-05-14 17:24:40 +0200

    added doc

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Fixed.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fvoicu <fvoicu@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: fvoicu <fvoicu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 11:02:09 by fvoicu            #+#    #+#             */
-/*   Updated: 2024/04/16 13:38:28 by fvoicu           ###   ########.fr       */
+/*   Updated: 2024/05/14 17:23:39 by fvoicu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,40 @@
 #include <string>
 #include "Fixed.hpp"
 
+/**
+ * @brief Default Constructor.
+ * 
+ * Initializes the fixed-point nubmer to zero.
+ * Prints a message indicating the default constructor
+ * is called.
+*/
 Fixed::Fixed() : value(0) {
   std::cout << "Default constructor called" << std::endl;
 }
 
+/**
+ * @brief Copy constructor.
+ * 
+ * Initializes the fixed-point number with the value of
+ * another Fixed object.
+ * Prints a message indicating the copy constuctor is called.
+ * 
+ * @param other The Fixed object to copy from.
+*/
 Fixed::Fixed(const Fixed& other) : value(other.value) {
   std::cout << "Copy constructor called" << std::endl;
 }
 
+/**
+ * @brief Copy assignment operator.
+ * 
+ * It assigns the value of another Fixed object to this one.
+ * Prints a message indicating the copy asignment operator
+ * is called.
+ * 
+ * @param other The Fixed object to get the value from.
+ * @return A reference to this Fixed object.
+*/
 Fixed& Fixed::operator=(const Fixed& other) {
   if (this != &other) {
     std::cout << "Copy assignment operator called" << std::endl;
@@ -30,19 +56,48 @@ Fixed& Fixed::operator=(const Fixed& other) {
   return *this;
 }
 
+/**
+ * @brief Destructor.
+ * 
+ * Releases all the allocated resources.
+ * Prints a message indicating the destructor is called.
+*/
 Fixed::~Fixed() {
   std::cout << "Destructor called" <<std::endl;
 }
 
+/**
+ * @brief Gets the raw bit representation of the fixed-point nbr.
+ * 
+ * Prints a message indicating this function is called.
+ * 
+ * @return The raw bits repersentation of the fixed-point nbr.
+*/
 int Fixed::getRawBits(void) const {
-  std::cout << "getRawBits member function called" << std::endl;
+  std::cout << "getRawBits member function called" \
+            << std::endl;
   return value;
 }
 
+/**
+ * @brief Sets the raw bit representaton of the fixed-point nbr.
+ * 
+ * Prints a message indicating this function is called.
+ * 
+ * @param raw The raw bits to set.
+*/
 void Fixed::setRawBits(int const raw) {
+  std::cout << "setRawBits member function called" \
+          << std::endl;
   value = raw;
 }
 
+/**
+ * @brief Prints the output value.
+ *
+ * A utility function to print the current value of the
+ * fixed-point number and the number of fractional bits.
+*/
 void Fixed::printOutput() {
   std::cout << value << std::endl;
   std::cout << frBits << std::endl;
--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -6,13 +6,24 @@
 /*   By: fvoicu <fvoicu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 10:53:36 by fvoicu            #+#    #+#             */
-/*   Updated: 2024/05/13 16:02:49 by fvoicu           ###   ########.fr       */
+/*   Updated: 2024/05/14 16:58:41 by fvoicu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_H
 # define FIXED_H
 
+/**
+ * @class Fixed
+ * @brief A class for reperesenting fixed-point numbers.
+ * 
+ * This class provides methods for handling fixed-point numbers
+ * with a specific number of fractional bits(8). It is written
+ * in the Orthodox Cannonical Form including a Default Constructor,
+ * a Copy Constructor, a Copy Assignment Operator and a Destructor,
+ * and methods to get and set the raw bit representation of the 
+ * number.
+*/
 class Fixed {
  private:
   int               value;
